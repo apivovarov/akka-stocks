7b88b4f090719444cb26197926a1ffec5cba2f28
retroryan <ryan@grandcloud.com>
2014-10-24 13:30:53 -0500

    adding stock actor persistence starting point

--- app/actors/StockActor.scala
+++ app/actors/StockActor.scala
@@ -1,22 +1,33 @@
 package actors
 
-import akka.actor.{Props, ActorRef, Actor}
+import akka.actor._
 import utils.FakeStockQuote
 import scala.collection.immutable.{HashSet, Queue}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import actors.StockManagerActor._
-import actors.StockActor.FetchLatest
+import actors.StockActor.{AddWatcherAfterRecover, Snap, FetchLatest}
 import scala.util.Random
-import java.io.Serializable
-import scala.Serializable
+import akka.persistence.PersistentActor
+
+import akka.pattern.ask
+import akka.util.Timeout
+import actors.StockManagerActor.UnwatchStock
+import actors.StockActor.AddWatcherAfterRecover
+import actors.StockManagerActor.WatchStock
+import actors.StockManagerActor.StockUpdate
+import akka.actor.ActorIdentity
+import akka.actor.Identify
+import actors.StockManagerActor.StockHistory
 
 /**
  * There is one StockActor per stock symbol.  The StockActor maintains a list of users watching the stock and the stock
  * values.  Each StockActor updates a rolling dataset of randomly generated stock values.
  */
 
-class StockActor(symbol: String) extends Actor {
+class StockActor(symbol: String) extends PersistentActor with ActorLogging {
+
+    implicit val identifyAskTimeout: Timeout = Duration(10, SECONDS)
 
     def persistenceId: String = {
         return "symbol_" + symbol
@@ -35,14 +46,18 @@ class StockActor(symbol: String) extends Actor {
     // Fetch the latest stock value every 75ms
     val stockTick = context.system.scheduler.schedule(Duration.Zero, 75.millis, self, FetchLatest)
 
-    def receive = {
+    val snapshotTick = context.system.scheduler.schedule(Duration.Zero, 75.millis, self, Snap)
+
+    override def receiveCommand: Receive = {
         case FetchLatest =>
             // add a new stock price to the history and drop the oldest
             val newPrice = FakeStockQuote.newPrice(stockHistory.last.doubleValue())
             stockHistory = stockHistory.drop(1) :+ newPrice
+            log.info(s"## Broadcasting stock update")
             // notify watchers
             watchers.foreach(_ ! StockUpdate(symbol, newPrice))
         case WatchStock(_) =>
+            log.info(s"## adding watch stock for: $sender")
             // send the stock history to the user
             sender ! StockHistory(symbol, stockHistory.toList)
             // add the watcher to the list
@@ -53,6 +68,25 @@ class StockActor(symbol: String) extends Actor {
                 stockTick.cancel()
                 context.stop(self)
             }
+        case AddWatcherAfterRecover(watcher) => watchers = watchers + watcher
+
+        case Snap => //todo take a snapshot
+    }
+
+
+    override def receiveRecover: Receive = {
+        case _ =>
+    }
+
+    private def addWatcherIfAlive(watcher: ActorRef) {
+        (watcher ? Identify(watcher.path.name)).mapTo[ActorIdentity].map {
+            actorIdentity =>
+                if (actorIdentity.getRef != null)
+                    self ! AddWatcherAfterRecover(watcher)
+        }.recover {
+            case failure =>
+                self ! UnwatchStock(Option(symbol))
+        }
     }
 }
 
@@ -63,15 +97,15 @@ object StockActor {
 
     case object FetchLatest
 
-    case class EventStockPriceUpdated(price:Double)
+    case class EventStockPriceUpdated(price: Double)
 
-    case class EventWatcherAdded(watcher:ActorRef)
+    case class EventWatcherAdded(watcher: ActorRef)
 
-    case class EventWatcherRemover(watcher:ActorRef)
+    case class EventWatcherRemover(watcher: ActorRef)
 
     case class TakeSnapshot(stockHistory: Queue[Double], watchers: HashSet[ActorRef])
 
-    case class AddWatcherAfterRecover(watcher:ActorRef)
+    case class AddWatcherAfterRecover(watcher: ActorRef)
 
     case object Snap
 
